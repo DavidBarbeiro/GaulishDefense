d64e80e34bf1dcad9758bd950789d6af7bf1fb75
David <david.gdbarbeiro@gmail.com>
2014-05-05 01:49:54 +0100

    Revert "Initial Commit"
    This reverts commit 57b1522e78fb49be487571e641a0affb2913a8d7.

--- Assets/Editor/EasyRoads3D/TerrainEditorScript.js
+++
@@ -1,9 +0,0 @@
-@CustomEditor(EasyRoads3DTerrainID)
-class TerrainEditorScript extends Editor
-{
-	function OnSceneGUI()
-	{
-		if(Event.current.shift && RoadObjectScript.slO != null) Selection.activeGameObject = RoadObjectScript.slO.gameObject;
-		else RoadObjectScript.slO = null;
-	}
-}
